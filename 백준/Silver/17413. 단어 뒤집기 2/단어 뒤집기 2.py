5fd7260135ff4791d37b85b335babc664d37d6e4
Seok99 <aixue1999@naver.com>
2024-03-21 16:59:47 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 1888 ms, Memory: 120384 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,30 +1,34 @@
-from sys import stdin
-from collections import deque
-k = stdin.readline().strip('\n')
-k = list(k)
-tmp = []
-flag = 0
-def substitution(k, index, tmp):
-    cnt = 0
-    for i in range(index-len(tmp), index):
-        k[i] = tmp[cnt]
+S = input()
+S = list(S)
+cnt = 0
+tmp = 0
+k = []
+def substitution(S, a, b, k):
+    k_ = 0
+    for i in range(a, b+1):
+        S[i] = k[k_]
+        k_ += 1
+
+for i in range(len(S)):
+    if S[i] == '<':
         cnt += 1
-    tmp.clear()
-for i in range(len(k)):
-    if k[i] == '<':
-        flag = 1
-        if len(tmp) > 0:
-            substitution(k, i, tmp)
+        if tmp > 0: #< 바로 앞에 문자열 있는 경우
+            substitution(S, i-tmp, i-1, k)
+            k.clear()
+            tmp = 0
         continue
-    elif k[i] == '>':
-        flag = 0
+    elif S[i] == '>':
+        cnt = 0
         continue
-    
-    if flag == 0:
-        if (k[i] >= 'a' and k[i] <= 'z') or (k[i] >= '0' and k[i] <= '9'):
-            tmp.insert(0, k[i])
-        else:
-            substitution(k, i, tmp)
-if len(tmp) > 0:
-    substitution(k, len(k), tmp)
-print(''.join(k))
+
+    if cnt == 0: #<> 사이의 문자가 아닌 경우
+        if S[i] != ' ':
+            k.insert(0, S[i])
+            tmp += 1
+        else: #공백의 경우 -> 뒤집기
+            substitution(S, i-tmp, i-1, k)
+            k.clear()
+            tmp = 0
+if tmp != 0:
+    substitution(S, len(S)-tmp, len(S)-1, k)
+print(''.join(S))
